db97fd7226d8c1d97c15166a7ea5b5dfb79a5375
Benegilliland <73527724+Benegilliland@users.noreply.github.com>
2020-11-04 19:23:24 +0000

    Add files via upload

---
+++ Encryptor.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+#include <fstream>
+
+char get_key();
+int encrypt_char(char& val, char ckey);
+//int decrypt_char(char& val, char ckey);
+int encrypt_string(std::string& val, char ckey);
+//int decrypt_string(std::string& val, char ckey);
+
+char get_key() {
+	srand(time(0)); // Generate random seed
+	char ckey = (char)(rand() % 256); // Generates random character
+	return ckey;
+}
+
+int encrypt_char(char& val, char ckey) {
+	val = ~val; // Reverse bits
+	val ^= ckey; // Bitwise XOR
+	val << 4; // Left shift 4 bits
+	val += 5;
+	return 0;
+}
+
+/*int decrypt_char(char& val, char ckey) {
+	val -= 5;
+	val ^= ckey;
+	val = ~val;
+	val >> 4;
+	return 0;
+}*/
+
+int encrypt_string(std::string& val, char ckey) {
+	for (int i = 0; i < val.length(); i++) {
+		char c = val[i];
+		encrypt_char(c, ckey);
+		val[i] = c;
+	}
+	return 0;
+}
+
+/*int decrypt_string(std::string& val, char ckey) {
+	for (int i = 0; i < val.length(); i++) {
+		char c = val[i];
+		decrypt_char(c, ckey);
+		val[i] = c;
+	}
+	return 0;
+}*/
